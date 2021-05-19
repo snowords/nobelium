ecddc25f399bdd96ef69eeaec7fa1179bba36b5d
张延航 <33chijian@gmail.com>
2021-05-19 14:34:51 +0800

    更新推特地址

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://twitter.com/Three__Sword',
+  socialLink: 'https://twitter.com/snoword1024',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
