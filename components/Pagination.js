a357d8063ec825faffeec3da648c3f3e232ad7c1
Snoword1024 <1430750200@qq.com>
2021-06-28 14:29:23 +0800

    D:\work-space\snoword\nobelium\:sparkles:拉取父仓库最新代码

--- components/Pagination.js
+++ components/Pagination.js
@@ -14,23 +14,27 @@ const Pagination = ({ page, showNext, nowPage }) => {
             : `/page/${currentPage - 1}`
         }
       >
-        <button
-          rel="prev"
-          className={`${
-            currentPage === 1 ? 'invisible' : 'block'
-          } cursor-pointer p-2 rounded-xl hover:bg-white hover:shadow transition-all duration-500 `}
-        >
-          ← {locale.PAGINATION.PREV}
-        </button>
+        <a>
+          <button
+            rel="prev"
+            className={`${
+              currentPage === 1 ? 'invisible' : 'block'
+            } cursor-pointer p-2 rounded-xl hover:bg-white hover:shadow transition-all duration-500 `}
+          >
+            ← {locale.PAGINATION.PREV}
+          </button>
+        </a>
       </Link>
       <p className="font-normal text-gray-500">{nowPage}</p>
       <Link href={`/page/${currentPage + 1}`}>
-        <button
-          rel="next"
-          className={`${+showNext ? 'block' : 'invisible'} cursor-pointer p-2 rounded-xl hover:bg-white hover:shadow transition-all duration-500 `}
-        >
-          {locale.PAGINATION.NEXT} →
-        </button>
+        <a>
+          <button
+            rel="next"
+            className={`${+showNext ? 'block' : 'invisible'} cursor-pointer p-2 rounded-xl hover:bg-white hover:shadow transition-all duration-500 `}
+          >
+            {locale.PAGINATION.NEXT} →
+          </button>
+        </a>
       </Link>
     </div>
   )
