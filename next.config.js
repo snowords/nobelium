a8af3e39e6dce1ef6ff62435643d80cb1011b168
snoword <33chijian@gmail.com>
2021-09-24 16:17:51 +0800

    :rocket:增加PWA功能

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
 module.exports = {
   webpack5: true,
   images: {
