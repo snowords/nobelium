524c91c3f54c245c3c85de4f45027821f9d51dc8
snoword <33chijian@gmail.com>
2021-10-10 22:56:20 +0800

    :rocket:增加版本页面

--- pages/changeLog.js
+++ pages/changeLog.js
@@ -1,22 +1,37 @@
 import Container from '@/components/Container'
 import { getChangeLog } from '@/lib/notion'
 
-export default function changeLog ({ changeData }) {
-  return (
-    <Container>
-      <h1>Change Log</h1>
-      {changeData.map((item, key) => 
-
-    </Container>
-  )
-}
-
 export async function getStaticProps () {
   const res = await getChangeLog()
   console.log(res.block)
+  const resData = []
+  for (const b in res.block) {
+    resData.push({
+      id: res.block[b].value.id,
+      created_time: res.block[b].value.created_time
+    })
+  }
   return {
     props: {
-      changeData: res.block
+      changeData: resData,
+      conunt: conunt
     }
   }
+}
+
+export default function changeLog ({ changeData, conunt }) {
+  return (
+    <Container>
+      <p>{conunt}</p>
+      {changeData.map((item, key) => {
+        return (
+          <div key={key}>
+            <p>{item.id}</p>
+            <p>{item.created_time}</p>
+            <p>{item.properties}</p>
+          </div>
+        )
+      })}
+    </Container>
+  )
 }
