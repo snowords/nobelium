ab88a2b3c4bfc2a5606daf261e3cc4e720190ec9
Snoword <1430750200@qq.com>
2021-12-07 09:51:07 +0800

    fix gitalk

--- pages/tools/Notes.js
+++ pages/tools/Notes.js
@@ -30,8 +30,7 @@ export default function Article () {
     setShowForm(!showForm)
   }
 
-  const onFinish = async (values) => {
-    console.log('values', values)
+  const onFinish = async (values) => { 
     if (!values.content) {
       message.error('内容不能为空！')
       return
