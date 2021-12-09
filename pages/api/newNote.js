7c9835d4a30e9930aa8017ac807efe308d3c98f1
Snoword <1430750200@qq.com>
2021-12-09 17:30:21 +0800

    修改bug

--- pages/api/newNote.js
+++ pages/api/newNote.js
@@ -1,9 +1,10 @@
 import graphcms from '@/lib/graphcms'
 
 export default async ({ body }, res) => {
+  // 有空格提交不上去
   const mutation = `
     mutation NewNote {
-      createNote(data: { title: "${body.title}", content: "${body.content}" }) {
+      createNote(data: { title: "${body.title.replace(/[\r\n]/g,"")}", content: "${body.content.replace(/[\r\n]/g,"")}" }) {
         id
       }
     }
